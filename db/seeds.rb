463072043567919190848a067de83e0b11d7ea0b
eagle111 <18645125206@sina.cn>
2017-04-19 22:39:49 +0800

    add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+u=User.new
+u.email="2@sina.cn"
+u.password="111111"
+u.password_confirmation="111111"
+u.is_admin=true
+u.save
+def seed_image(file_name)
+  File.open(File.join(Rails.root, "/app/assets/images/#{file_name}.jpg"))
+end
