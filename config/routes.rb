daafece4c87e07b25ad99f547cfcc36e8441ee60
eagle111 <18645125206@sina.cn>
2017-04-21 12:27:24 +0800

    serach

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  resources :jobs do
+    collection do
+    get :search
+  end
+  resources :resumes
+end
   namespace :admin do
     resources :jobs do
       member do
@@ -9,10 +14,9 @@ Rails.application.routes.draw do
       end
       resources :resumes
     end
+
   end
-    resources :jobs do
-    resources :resumes
-  end
+
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
