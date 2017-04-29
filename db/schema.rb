471e6f0c5a4bdc38721103064aaf693cd8ceb126
eagle111 <18645125206@sina.cn>
2017-04-29 10:17:25 +0800

    除边框

--- db/schema.rb
+++ db/schema.rb
@@ -19,13 +19,6 @@ ActiveRecord::Schema.define(version: 20170426044450) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "job_relationships", force: :cascade do |t|
-    t.integer  "job_id"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
